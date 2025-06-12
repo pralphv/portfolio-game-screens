9cc7478b9db143edead95646718af333e623028e
pralphv <chowzhiho321@gmail.com>
2025-06-12 09:45:35 -0400

    more progress

--- src/routes/aboutMe/index.tsx
+++ src/routes/aboutMe/index.tsx
@@ -26,11 +26,98 @@ const RightPanel = ({
           <div className="bullet-point" style={{ marginLeft: "0.5em" }} />
           <h2>{title}</h2>
         </div>
-        <div className="panel">{children}</div>
+        <div className="panel" style={{ height: "56vh" }}>
+          {children}
+        </div>
       </>
     )
   );
 };
+
+const Me = () => (
+  <RightPanel title="Me">
+    <p>
+      I'm your typical dev doing mundane tasks on the job. There's not as much
+      cool things to do when you're in corporate. So here I am coding something
+      probably no one will see, but I'm doing it anyways because its fun and I
+      get to learn something that is not Leetcode.
+    </p>
+  </RightPanel>
+);
+
+const ThisPage = () => (
+  <RightPanel title="This Page">
+    <div style={{ display: "flex", gap: "1em", flexDirection: "column" }}>
+      <p>I want to recreate cool screens from games.</p>
+      <p>
+        Currently this is NieR: Automata's title screen and pause screen.
+        Eventually I want to recreate more game screen. Like Persona's.
+      </p>
+      <p>Ultimately, my goal is to learn animations and GSAP.</p>
+    </div>
+  </RightPanel>
+);
+
+const TechStack = () => (
+  <RightPanel title="Tech Stack">
+    <div style={{ display: "flex", gap: "1em", flexDirection: "column" }}>
+      <p>
+        I started with Python and that has been my main language throughout my
+        career. I've been finding myself preferring TypeScript for backends for
+        web development. Running the same language for the back and front does
+        make things easier.
+      </p>
+      <p>
+        Scripting-wise, it just depends on what I'm trying to do. If its data
+        analyzing, clearly its going to be Python Polars. If its scraping, then
+        Node Playwright.
+      </p>
+      <p>
+        React is my choice of frontend. Although I have been meaning to get into
+        Svelte.
+      </p>
+      <p>
+        My job uses GraphQl but honestly it adds more complexity for not a lot
+        of benefits.
+      </p>
+    </div>
+  </RightPanel>
+);
+
+const AmIaWeeb = () => (
+  <RightPanel title="Am I a weeb">
+    <div style={{ display: "flex", gap: "1em", flexDirection: "column" }}>
+      <p>No I'm not. I just happen to have watched probably over 200 anime.</p>
+      <p>And no I'm not learning Japanese because of anime.</p>
+    </div>
+  </RightPanel>
+);
+
+const HowThisWasMade = () => (
+  <RightPanel title="How this was made">
+    <div style={{ display: "flex", gap: "1em", flexDirection: "column" }}>
+      <p>Landing page: @pixi/react</p>
+      <p>
+        Others: React with lots of css. No SSR since this page doesn't care for
+        SEO.
+      </p>
+      <p>
+        Got some css started from{" "}
+        <a
+          href="https://codepen.io/levise/pen/vMzEwr"
+          style={{ display: "inline", padding: 0, fontWeight: 600 }}
+        >
+          this codepen
+        </a>
+      </p>
+      <p>
+        Don't bother checking the code because its a shithole. I did not intend
+        it to be maintanable. I just coded something out quickly because I have
+        other chores to do. I don't code like this at work I promise.
+      </p>
+    </div>
+  </RightPanel>
+);
 
 const LeftPanel = () => {
   const { section } = useParams();
@@ -57,6 +144,7 @@ const LeftPanel = () => {
           createUrl("tech_stack", "Tech Stack"),
           createUrl("am_i_a_weeb", "Am I a weeb"),
           createUrl("how_this_was_made", "How this was made"),
+          createUrl("contact", "Contact"),
         ].map((obj) => (
           <li
             key={obj.url}
@@ -78,6 +166,20 @@ const LeftPanel = () => {
   );
 };
 
+const Contact = () => (
+  <RightPanel title="Contact">
+    <a href="mailto:pralphv321@gmail.com" style={{ fontWeight: 600 }}>
+      pralphv321@gmail.com
+    </a>
+    <a
+      href="https://www.linkedin.com/in/ralph-vincent-o-perez-16b378122"
+      style={{ fontWeight: 600 }}
+    >
+      LinkedIn
+    </a>
+  </RightPanel>
+);
+
 const TITLE = "About Me";
 const AboutMe = () => {
   const ready = useDelayIfRefresh(500);
@@ -92,31 +194,12 @@ const AboutMe = () => {
             <div style={{ display: "flex", gap: "2em", flex: "1" }}>
               <LeftPanel />
               <div style={{ flex: "1" }}>
-                {section === "me" && (
-                  <RightPanel title="Me">
-                    <div>me</div>
-                  </RightPanel>
-                )}
-                {section === "this_page" && (
-                  <RightPanel title="This page">
-                    <div>this page</div>
-                  </RightPanel>
-                )}
-                {section === "tech_stack" && (
-                  <RightPanel title="Tech stack">
-                    <div>tech stack</div>
-                  </RightPanel>
-                )}
-                {section === "am_i_a_weeb" && (
-                  <RightPanel title="Am I a weeb">
-                    <div>am i a weeb</div>
-                  </RightPanel>
-                )}
-                {section === "how_this_was_made" && (
-                  <RightPanel title="How this was made">
-                    <div>how this was made</div>
-                  </RightPanel>
-                )}
+                {section === "me" && <Me />}
+                {section === "this_page" && <ThisPage />}
+                {section === "tech_stack" && <TechStack />}
+                {section === "am_i_a_weeb" && <AmIaWeeb />}
+                {section === "how_this_was_made" && <HowThisWasMade />}
+                {section === "contact" && <Contact />}
               </div>
             </div>
           </div>
